488ae9d781fc699877a773d3d6ecbc3d0d03a5f5
fcl <fcl@qq.com>
2023-02-15 21:16:32 +0800

    项目初始化

---
+++ mall-admin/src/main/java/com/wwx/teamall/TeaMallAdminApplication.java
@@ -0,0 +1,11 @@
+package com.wwx.teamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaMallAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeaMallAdminApplication.class, args);
+    }
+}
